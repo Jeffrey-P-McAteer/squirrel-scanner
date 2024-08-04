df6b2947fa6e82e49e4848f3a88ca58fccf1c838
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-08-04 08:41:55 -0400

    beginning of model training; we will also need to capture + store marked up images in some manner (TODO research existing standards)

---
+++ model-trainer.py
@@ -0,0 +1,26 @@
+
+import os
+import sys
+import subprocess
+import shutil
+
+site_packages = os.path.join(os.path.dirname(__file__), 'model-site-packages')
+os.makedirs(site_packages, exist_ok=True)
+sys.path.append(site_packages)
+
+try:
+  import ultralytics
+except:
+  subprocess.run([
+    sys.executable, '-m', 'pip', 'install', f'--target={site_packages}', 'ultralytics'
+  ])
+  import ultralytics
+
+
+print(f'ultralytics = {ultralytics}')
+ultralytics.checks()
+
+import ultralytics.YOLO
+print(f'ultralytics.YOLO = {ultralytics.YOLO}')
+
+
