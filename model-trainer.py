85791b14aab4a23121e93d80899d74425f133bb0
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-08-04 11:34:35 -0400

    moved python site-packages to scratch disk iff laptop is at desk

--- model-trainer.py
+++ model-trainer.py
@@ -5,6 +5,10 @@ import subprocess
 import shutil
 
 site_packages = os.path.join(os.path.dirname(__file__), 'model-site-packages')
+
+if os.path.exists('/mnt/scratch/tmp'): # Jeff's laptop, site-packages ends up being 4.5gb!!! so it goes on the scratch disk.
+  site_packages = '/mnt/scratch/squirrel-scanner/model-site-packages'
+
 os.makedirs(site_packages, exist_ok=True)
 sys.path.append(site_packages)
 
